96e97b983ceb8421130be94d93f62946684ba843
Claire McShane <cemcshane@wustl.edu>
2019-11-08 00:29:59 -0600

    usernames working

--- chat-server.js
+++ chat-server.js
@@ -21,11 +21,16 @@ app.listen(3456);
 var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
-	
+    socket.on('name_to_server', function(data) {
+		// This callback runs when the server receives a new message from the client.
+        console.log("nickname: "+data["nickname"]); // log it to the Node.JS output
+        socket.nickname = data["nickname"];
+		io.sockets.emit("name_to_client",{nickname:data["nickname"] }); // broadcast the message to other users
+    });
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
 		
-		console.log("message: "+data["message"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
-	});
+        console.log("message: "+data["message"]); // log it to the Node.JS output
+		io.sockets.emit("message_to_client",{message:data["message"], user:socket.nickname}) // broadcast the message to other users
+    });
 });
