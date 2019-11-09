88f4151eda8ad5f63d91d3b0790f92ef6d1cca12
Claire McShane <cemcshane@wustl.edu>
2019-11-09 00:44:40 -0600

    basic rooms working

--- chat-server.js
+++ chat-server.js
@@ -21,28 +21,32 @@ app.listen(3456);
 var io = socketio.listen(app);
 
 io.sockets.on("connection", function(socket){
-	// This callback runs when a new Socket.IO connection is established.
+    // This callback runs when a new Socket.IO connection is established.
     socket.on('name_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
         console.log("nickname: "+data["nickname"]); // log it to the Node.JS output
         socket.nickname = data["nickname"];
+        socket.join("Main Chat Room");
 		io.sockets.emit("name_to_client",{nickname:data["nickname"] }); // broadcast the message to other users
     });
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
         console.log("message: "+data["message"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client",{message:data["message"], user:socket.nickname}) // broadcast the message to other users
+		io.in(data["room"]).emit("message_to_client",{message:data["message"], user:socket.nickname}) // broadcast the message to other users
     });
     socket.on('room_to_server', function(data) {
         // This callback runs when the server receives a new message from the client.
-        socket.join(data["newroom"]);        
+        socket.join(data["newroom"]);
+        socket.leave(data["curr"]);     
         console.log("Room joined: "+data["newroom"]); // log it to the Node.JS output
 		io.sockets.emit("room_to_client",{room:data["newroom"], user:socket.nickname}) // broadcast the message to other users
+        socket.emit("joinroom_to_client",{room:data["newroom"], user:socket.nickname})
     });
     socket.on('joinroom_to_server', function(data) {
         // This callback runs when the server receives a new message from the client.
-        socket.join(data["joinroom"]);        
+        socket.join(data["joinroom"]);
+        socket.leave(data["curr"]);      
         console.log("Room joined: "+data["joinroom"]); // log it to the Node.JS output
-		io.sockets.emit("joinroom_to_client",{room:data["joinroom"], user:socket.nickname}) // broadcast the message to other users
+		socket.emit("joinroom_to_client",{room:data["joinroom"], user:socket.nickname}) // broadcast the message to other users
     });
 });
