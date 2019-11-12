ccfdd3b0ff803d7751578e49c9b132d852c9e3a1
Claire McShane <cemcshane@wustl.edu>
2019-11-12 00:40:28 -0600

    displaying to admin

--- chat-server.js
+++ chat-server.js
@@ -27,6 +27,7 @@ let pwds = new Map();
 let prvs = new Map();
 let roomId = 0;
 let map = new Map();
+let creators = new Map();
 map.set("Main Chat Room", 0);
 prvs.set("Main Chat Room", "no");
 io.sockets.on("connection", function(socket){
@@ -45,7 +46,7 @@ io.sockets.on("connection", function(socket){
 		io.in(data["room"]).emit("message_to_client",{message:data["message"], user:socket.nickname}) // broadcast the message to other users
     });
     socket.on('room_to_server', function(data) {
-        // This callback runs when the server receives a new message from the client.
+        creators.set(data["newroom"], socket.nickname);
         if(data["prv"]=="yes"){
             pwds.set(data["newroom"], data["pwd"]);
             prvs.set(data["newroom"], "yes");
@@ -63,9 +64,9 @@ io.sockets.on("connection", function(socket){
         mems.push(arry);
         console.log("Room joined: "+data["newroom"]); // log it to the Node.JS output
 		io.sockets.emit("room_to_client",{room:data["newroom"], user:socket.nickname}) // broadcast the message to other users
-        socket.emit("joinroom_to_client",{room:data["newroom"], change:"yes", user:socket.nickname, users:mems[map.get(data["newroom"])]});
-        io.to(data["newroom"]).emit("joinroom_to_client",{room:data["newroom"], change:"no", user:socket.nickname, users:mems[map.get(data["newroom"])]});
-        io.to(data["curr"]).emit("joinroom_to_client",{room:data["curr"], change:"no", user:socket.nickname, users:mems[map.get(data["curr"])]});
+        socket.emit("joinroom_to_client",{creator:"yes", room:data["newroom"], change:"yes", user:socket.nickname, users:mems[map.get(data["newroom"])]});
+        io.to(data["newroom"]).emit("joinroom_to_client",{creator:"", room:data["newroom"], change:"no", user:socket.nickname, users:mems[map.get(data["newroom"])]});
+        io.to(data["curr"]).emit("joinroom_to_client",{creator:"", room:data["curr"], change:"no", user:socket.nickname, users:mems[map.get(data["curr"])]});
     });
     socket.on('pass_to_server', function(data) {
         if(prvs.get(data["joinroom"])=="yes"){
@@ -86,13 +87,20 @@ io.sockets.on("connection", function(socket){
     });
     socket.on('joinroom_to_server', function(data) {
         // This callback runs when the server receives a new message from the client.
+        let crtr;
+        if(creators.get(data["joinroom"])==socket.nickname){
+            crtr = "yes";
+        }
+        else{
+            crtr = "no";
+        }
         socket.join(data["joinroom"]);
         socket.leave(data["curr"]);
         mems[map.get(data["curr"])] = mems[map.get(data["curr"])].filter(name => name!=socket.nickname);
         mems[map.get(data["joinroom"])].push(socket.nickname);
         console.log("Room joined: "+data["joinroom"]); // log it to the Node.JS output
-		socket.emit("joinroom_to_client",{change:"yes", room:data["joinroom"], user:socket.nickname, users:mems[map.get(data["joinroom"])]}); // broadcast the message to other users
-        io.to(data["joinroom"]).emit("joinroom_to_client",{room:data["joinroom"], change:"no", user:socket.nickname, users:mems[map.get(data["joinroom"])]});
-        io.to(data["curr"]).emit("joinroom_to_client",{room:data["curr"], change:"no", user:socket.nickname, users:mems[map.get(data["curr"])]});
+		socket.emit("joinroom_to_client",{creator:crtr, change:"yes", room:data["joinroom"], user:socket.nickname, users:mems[map.get(data["joinroom"])]}); // broadcast the message to other users
+        io.to(data["joinroom"]).emit("joinroom_to_client",{creator:"", room:data["joinroom"], change:"no", user:socket.nickname, users:mems[map.get(data["joinroom"])]});
+        io.to(data["curr"]).emit("joinroom_to_client",{creator:"", room:data["curr"], change:"no", user:socket.nickname, users:mems[map.get(data["curr"])]});
     });
 });
